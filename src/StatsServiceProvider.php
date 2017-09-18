8bf362bdaea1004f518a1fe78cf6d56a5eaefe8e
Stefan Zweifel <hello@stefanzweifel.io>
2017-09-18 20:57:25 +0200

    Add StatsService Provider

---
+++ src/StatsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wnx\LaravelStats;
+
+use Illuminate\Support\ServiceProvider;
+
+class StatsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+
+}
