8fe2c659ba2e467fe9aca62439d43a46f7c995e6
Stefan Zweifel <hello@stefanzweifel.io>
2017-10-01 21:34:37 +0200

    Add own ReflectionClass implementation

---
+++ src/ReflectionClass.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Wnx\LaravelStats;
+
+use ReflectionClass as NativeReflectionClass;
+use Wnx\LaravelStats\ComponentConfiguration;
+
+class ReflectionClass
+{
+    /**
+     * @var ReflectionClass
+     */
+    protected $class;
+
+    public function __construct($className)
+    {
+        $this->class = new NativeReflectionClass($className);
+    }
+
+    public function getLaravelComponentName()
+    {
+        if ($componentName = $this->extendsLaravelComponentClass($this->class)) {
+            return $componentName;
+        } elseif ($componentName = $this->usesLaravelComponentTrait($this->class)) {
+            return $componentName;
+        }
+
+        return null;
+    }
+
+    public function isLaravelComponent()
+    {
+        if ($componentName = $this->extendsLaravelComponentClass($this->class)) {
+            return true;
+        } elseif ($componentName = $this->usesLaravelComponentTrait($this->class)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get Component Configuration.
+     *
+     * @return Collection
+     */
+    protected function componentConfiguration()
+    {
+        return resolve(ComponentConfiguration::class)->get();
+    }
+
+    /**
+     * Determine if given Class extends from a Core Laravel Class.
+     *
+     * @param ReflectionClass $reflection
+     *
+     * @return mixed (string|boolean)
+     */
+    protected function extendsLaravelComponentClass(\ReflectionClass $reflection)
+    {
+        // Check if Classname of currently given Class is in Extends Array
+        $extends = $this->componentConfiguration()->pluck('extends', 'name');
+
+        $className = $reflection->getName();
+        $componentName = $extends->search($className);
+
+        // Found current Class Name, return the Component Name
+        if ($componentName !== false) {
+            return $componentName;
+        }
+
+        // Does the Class have a Parent Class?
+        // If yes, recursivly call this method
+        $hasParentClass = $reflection->getParentClass();
+
+        if ($hasParentClass !== false) {
+            return $this->extendsLaravelComponentClass($hasParentClass);
+        }
+
+        // If no, return false
+        return false;
+    }
+
+    /**
+     * Determine if a given Class uses a Laravel Core Trait.
+     *
+     * @param ReflectionClass $reflection
+     *
+     * @return mixed (string|boolean)
+     */
+    protected function usesLaravelComponentTrait(\ReflectionClass $reflection)
+    {
+        // If the given Class does not use any traits, return false
+        if (count($reflection->getTraits()) == 0) {
+            return false;
+        }
+
+        $uses = $this->componentConfiguration()->pluck('uses', 'name');
+
+        $classTraits = $reflection->getTraitNames();
+        $componentName = false;
+
+        // Loop through all traits and search Trait in Component Configuration
+        foreach ($classTraits as $trait) {
+            if ($uses->search($trait)) {
+                $componentName = $uses->search($trait);
+            }
+        }
+
+        // If Trait has been found, we return the Component Name
+        if ($componentName !== false) {
+            return $componentName;
+        }
+
+        // Does the Class have a Parent Class?
+        // If yes, recursivly call this method
+        $hasParentClass = $reflection->getParentClass();
+
+        if ($hasParentClass !== false) {
+            return $this->usesLaravelComponentTrait($hasParentClass);
+        }
+
+        return false;
+    }
+}
