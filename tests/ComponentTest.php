1e109279bede0e2bb076eda7b54f5dcafa6dabe5
Stefan Zweifel <hello@stefanzweifel.io>
2017-10-03 20:21:58 +0200

    Write Component Tests

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -2,9 +2,29 @@
 
 namespace Wnx\LaravelStats\Tests;
 
+use Wnx\LaravelStats\Component;
+use Wnx\LaravelStats\Tests\Stubs\Events\DemoEvent;
 use Wnx\LaravelStats\Tests\TestCase;
 
 class ComponentTest extends TestCase
 {
-    //
+    /** @test */
+    public function it_sets_component_name()
+    {
+        $component = new Component;
+        $component->setName('FooBar');
+
+        $this->assertEquals('FooBar', $component->getName());
+    }
+
+    /** @test */
+    public function it_sets_classes()
+    {
+        $component = new Component;
+        $component->setClasses([
+            DemoEvent::class
+        ]);
+
+        $this->assertTrue(is_array($component->getClasses()));
+    }
 }
