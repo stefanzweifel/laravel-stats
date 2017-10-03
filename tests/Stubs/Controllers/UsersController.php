e14939d49a701d0be86f3f54d5c6dc9362a6ad39
Stefan Zweifel <hello@stefanzweifel.io>
2017-10-03 20:47:11 +0200

    Add a UsersController Stub

---
+++ tests/Stubs/Controllers/UsersController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Stubs\Controllers;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        // code
+    }
+
+    public function create()
+    {
+        // code
+    }
+
+    public function store()
+    {
+        // code
+    }
+
+    public function show()
+    {
+        // code
+    }
+
+    public function edit()
+    {
+        // code
+    }
+
+    public function update()
+    {
+        // code
+    }
+
+    public function delete()
+    {
+        // code
+    }
+
+}
