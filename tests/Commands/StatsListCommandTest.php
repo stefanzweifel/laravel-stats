d7628f628ca990d0e6f4adc245fbad8a3bda78eb
Stefan Zweifel <hello@stefanzweifel.io>
2017-10-08 15:07:32 +0200

    Add Test for StatsListCommand

---
+++ tests/Commands/StatsListCommandTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Commands;
+
+use Illuminate\Support\Facades\Artisan;
+use Wnx\LaravelStats\Commands\StatsListCommand;
+use Wnx\LaravelStats\Tests\TestCase;
+
+class StatisticsListCommandTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $this->artisan('stats');
+        $resultAsText = Artisan::output();
+
+        $this->assertContains('Controllers', $resultAsText);
+    }
+}
