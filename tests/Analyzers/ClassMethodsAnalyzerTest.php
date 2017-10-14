071d78318c5a93614ae8cadb3dfc3fba1d2155f0
Jergus Lejko <jergus.lejko@gmail.com>
2017-10-14 23:55:51 +0100

    Clean up getMethods method

--- tests/Analyzers/ClassMethodsAnalyzerTest.php
+++ tests/Analyzers/ClassMethodsAnalyzerTest.php
@@ -11,19 +11,6 @@ use Wnx\LaravelStats\Tests\TestCase;
 class ClassMethodsAnalyzerTest extends TestCase
 {
     /** @test */
-    public function it_returns_the_names_of_all_methods_declared_on_a_class()
-    {
-        $service = resolve(ClassMethodsAnalyzer::class);
-        $reflection = new ReflectionClass(ProjectsController::class);
-
-        $methodsCollection = $service->getCollectionOfMethodNames($reflection);
-
-        $this->assertContains('index', $methodsCollection->toArray());
-        $this->assertContains('show', $methodsCollection->toArray());
-        $this->assertContains('protectedControllerMethod', $methodsCollection->toArray());
-    }
-
-    /** @test */
     public function it_returns_the_number_of_methods_declared_on_a_class()
     {
         $service = resolve(ClassMethodsAnalyzer::class);
