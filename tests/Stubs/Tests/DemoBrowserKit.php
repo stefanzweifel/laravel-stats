decd10173033c15d1239022fbcdc1a2658861ba9
Stefan Zweifel <hello@stefanzweifel.io>
2017-10-23 20:58:58 +0200

    Add a demo BrowserKit Test

---
+++ tests/Stubs/Tests/DemoBrowserKit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Stubs\Tests;
+
+use Laravel\BrowserKitTesting\TestCase;
+
+class DemoBrowserKit extends TestCase
+{
+    public function createApplication()
+    {
+        //
+    }
+
+    /** @test */
+    public function it_runs_browser_kit_tests()
+    {
+        $this->assertTrue(true);
+    }
+}
