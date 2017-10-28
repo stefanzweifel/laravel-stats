b318985c05d608f4fc1080b18becb1dceb7656fe
Stefan Zweifel <hello@stefanzweifel.io>
2017-10-28 12:25:00 +0200

    Add DemoEventListener and EventServiceProvider

---
+++ tests/Stubs/EventListeners/DemoEventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Stubs\EventListeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DemoEventListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+    }
+}
