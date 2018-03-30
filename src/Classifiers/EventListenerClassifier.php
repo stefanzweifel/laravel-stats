032839496307f6eda8a59776d92dbf2ee2cc8b66
Stefan Zweifel <hello@stefanzweifel.io>
2018-03-30 11:53:20 +0200

    Refactor EventListenerClassifier

--- src/Classifiers/EventListenerClassifier.php
+++ src/Classifiers/EventListenerClassifier.php
@@ -5,8 +5,8 @@ namespace Wnx\LaravelStats\Classifiers;
 use Closure;
 use ReflectionFunction;
 use ReflectionProperty;
-use Wnx\LaravelStats\ReflectionClass;
 use Wnx\LaravelStats\Contracts\Classifier;
+use Wnx\LaravelStats\ReflectionClass;
 
 class EventListenerClassifier implements Classifier
 {
@@ -17,22 +17,32 @@ class EventListenerClassifier implements Classifier
 
     public function satisfies(ReflectionClass $class)
     {
-        $dispatcher = app('events');
-
-        $property = new ReflectionProperty($dispatcher, 'listeners');
-        $property->setAccessible(true);
-
-        return collect($property->getValue($dispatcher))
+        return collect($this->getEvents())
             ->map(function ($listeners) {
                 $subscriber = collect($listeners)->map(function (Closure $closure) {
-                    $reflection = new ReflectionFunction($closure);
-
-                    return $reflection->getStaticVariables()['listener'];
+                    return $this->getEventListener($closure);
                 })->toArray();
 
                 return $subscriber;
             })->collapse()
             ->unique()
             ->contains($class->getName());
+    }
+
+    protected function getEvents()
+    {
+        $dispatcher = app('events');
+
+        $property = new ReflectionProperty($dispatcher, 'listeners');
+        $property->setAccessible(true);
+
+        return $property->getValue($dispatcher);
+    }
+
+    protected function getEventListener(Closure $closure)
+    {
+        $reflection = new ReflectionFunction($closure);
+
+        return $reflection->getStaticVariables()['listener'];
     }
 }
