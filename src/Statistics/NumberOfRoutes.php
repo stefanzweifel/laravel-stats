bc25c52325e006a697c9ecbad56f907152919ba0
Stefan Zweifel <hello@stefanzweifel.io>
2019-03-16 15:13:06 +0100

    Add NumberOfRoutes

---
+++ src/Statistics/NumberOfRoutes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wnx\LaravelStats\Statistics;
+
+class NumberOfRoutes
+{
+    public function get() : int
+    {
+        return rescue(function ()  {
+            return collect(app('router')->getRoutes())->count();
+        }, 0);
+    }
+}
---
+++ tests/Statistics/NumberOfRoutesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Statistics;
+
+use Illuminate\Support\Facades\Route;
+use Wnx\LaravelStats\Statistics\NumberOfRoutes;
+use Wnx\LaravelStats\Tests\TestCase;
+
+class NumberOfRoutesTest extends TestCase
+{
+    /** @test */
+    public function it_returns_0_if_no_routes_are_registered()
+    {
+        $result = resolve(NumberOfRoutes::class)->get();
+
+        $this->assertEquals(0, $result);
+    }
+
+    /** @test */
+    public function it_returns_the_number_of_registered_routes()
+    {
+        Route::get('users', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@index');
+        Route::get('users/create', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@create');
+        Route::post('users', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@store');
+        Route::get('users/{user}', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@show');
+        Route::get('users/{user}/edit', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@edit');
+        Route::patch('users/{user}', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@update');
+        Route::delete('users/{user}', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@destroy');
+
+        $result = resolve(NumberOfRoutes::class)->get();
+
+        $this->assertEquals(7, $result);
+    }
+
+    /** @test */
+    public function it_only_counts_unique_routes()
+    {
+        Route::get('users', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@index');
+        Route::get('users', 'Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController@create');
+
+        $result = resolve(NumberOfRoutes::class)->get();
+
+        $this->assertEquals(1, $result);
+    }
+
+}
