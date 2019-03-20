b683551aeede3c159ff1b3e3defe57da3f86c587
AdrianHL <16022257+AdrianHL@users.noreply.github.com>
2019-03-20 08:31:22 +0000

    New Route Middleware
    Add a new Middleware in order to test

---
+++ tests/Stubs/Middlewares/RouteMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Stubs\Middlewares;
+
+use Closure;
+
+class RouteMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
