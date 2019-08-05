f4527fa80979bea57c2a1ef7b8e758bde71bb635
Stefan Zweifel <hello@stefanzweifel.io>
2019-08-05 20:52:09 +0200

    Add ComponentClass VO

---
+++ src/ValueObjects/ComponentClass.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Wnx\LaravelStats\ValueObjects;
+
+use Illuminate\Contracts\Support\Arrayable;
+use SebastianBergmann\PHPLOC\Analyser;
+use Wnx\LaravelStats\Contracts\Classifier;
+use Wnx\LaravelStats\ReflectionClass;
+
+class ComponentClass implements Arrayable
+{
+    /**
+     * @var \Wnx\LaravelStats\ReflectionClass
+     */
+    private $reflectionClassInstance;
+
+    /**
+     * ahdjkashd
+     * @var \Wnx\LaravelStats\Contracts\Classifier
+     */
+    private $classifier;
+
+    public function __construct(ReflectionClass $reflectionClassInstance, Classifier $classifier)
+    {
+        $this->reflectionClassInstance = $reflectionClassInstance;
+        $this->classifier = $classifier;
+    }
+
+    /**
+     * Return the total number of Methods declared in all declared classes.
+     *
+     * @return int
+     */
+    public function getNumberOfMethods(): int
+    {
+        return $this->reflectionClassInstance->getDefinedMethods()->count();
+
+        return $this->classes
+            ->sum(function (ReflectionClass $class) {
+                return $class->getDefinedMethods()->count();
+            });
+    }
+
+    /**
+     * Return the total number of lines.
+     *
+     * @return int
+     */
+    public function getLines(): int
+    {
+        return app(Analyser::class)
+            ->countFiles([$this->reflectionClassInstance->getFileName()], false)['loc'];
+    }
+
+    /**
+     * Return the total number of lines of code.
+     *
+     * @return float
+     */
+    public function getLogicalLinesOfCode(): float
+    {
+        return app(Analyser::class)
+            ->countFiles([$this->reflectionClassInstance->getFileName()], false)['lloc'];
+
+        return $this->classes
+            ->map(function (ReflectionClass $class) {
+                return $class->getFileName();
+            })
+            ->pipe(function (Collection $classes) {
+                return app(Analyser::class)->countFiles($classes->all(), false)['lloc'];
+            });
+    }
+
+    /**
+     * Return the average number of lines of code per method.
+     *
+     * @return float
+     */
+    public function getLogicalLinesOfCodePerMethod(): float
+    {
+        if ($this->getNumberOfMethods() === 0) {
+            return 0;
+        }
+
+        return round($this->getLogicalLinesOfCode() / $this->getNumberOfMethods(), 2);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'component_name' => $this->classifier->name(),
+            'methods' => $this->getNumberOfMethods(),
+            'loc' => $this->getLines(),
+            'lloc' => $this->getLogicalLinesOfCode(),
+            'lloc_per_method' => $this->getLogicalLinesOfCodePerMethod()
+        ];
+    }
+
+}
