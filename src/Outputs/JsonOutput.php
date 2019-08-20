dbbee934a5414baca6b7088a3ec702ecb9813c41
Stefan Zweifel <hello@stefanzweifel.io>
2019-08-20 19:55:28 +0200

    Add ugly JsonOutput v2

--- src/Outputs/JsonOutput.php
+++ src/Outputs/JsonOutput.php
@@ -3,13 +3,13 @@
 namespace Wnx\LaravelStats\Outputs;
 
 use Illuminate\Console\OutputStyle;
+use Illuminate\Support\Collection;
 use Wnx\LaravelStats\Project;
 use Wnx\LaravelStats\Statistics\JsonBuilder;
+use Wnx\LaravelStats\Statistics\NumberOfRoutes;
 use Wnx\LaravelStats\Statistics\ProjectStatistics;
+use Wnx\LaravelStats\ValueObjects\ClassifiedClass;
 
-/**
- * @deprecated
- */
 class JsonOutput
 {
     /**
@@ -29,56 +29,101 @@ class JsonOutput
         $this->output = $output;
     }
 
-    public function render(Project $project)
+    public function render(Project $project, bool $isVerbose = false)
     {
+        $codeLloc = $project->getAppCodeLogicalLinesOfCode();
+        $testsLloc = $project->getTestsCodeLogicalLinesOfCode();
+
         $jsonStructure = [
-            'components' => [
-                [
-                    'name' => 'String',
-                    'number_of_classes' => 0,
-                    'methods' => 0,
-                    'methods_per_class' => 0,
-                    'loc' => 0,
-                    'lloc' => 0,
-                    'lloc_per_method' => 0,
-
-                    // If isVerbose
-                    'classes' => [
-                        [
-                            'name' => 'FQDN',
-                            'methods' => 0,
-                            'methods_per_class' => 0,
-                            'loc' => 0,
-                            'lloc' => 0,
-                            'lloc_per_method' => 0,
-                        ]
-                    ],
-
-                ],
-            ],
+            'components' => [],
             'total' => [
-                'number_of_classes' => 0,
-                'methods' => 0,
-                'methods_per_class' => 0,
-                'loc' => 0,
-                'lloc' => 0,
-                'lloc_per_method' => 0,
+                'number_of_classes' => $project->getNumberOfClasses(),
+                'number_of_methods' => $project->getNumberOfMethods(),
+                'methods_per_class' => $project->getNumberOfMethodsPerClass(),
+                'loc' => $project->getLinesOfCode(),
+                'lloc' => $project->getLogicalLinesOfCode(),
+                'lloc_per_method' => $project->getLogicalLinesOfCodePerMethod()
             ],
             'meta' => [
-                'code_lloc' => 0,
-                'test_lloc' => 0,
-                'code_to_test_ratio' => 0,
-                'number_of_routes' => 0,
-            ]
+                'code_lloc' => $codeLloc,
+                'test_lloc' => $testsLloc,
+                'code_to_test_ratio' => ($testsLloc/$codeLloc),
+                'number_of_routes' => app(NumberOfRoutes::class)->get(),
+            ],
         ];
 
-        $this->output->text(json_encode($jsonStructure, JSON_PRETTY_PRINT));
+        $groupedByComponent = $this->groupClassesByComponentName($project);
 
+        foreach ($groupedByComponent as $componentName => $classifiedClasses) {
+
+            $numberOfClasses = $classifiedClasses->count();
+
+            $numberOfMethods = $classifiedClasses->sum(function (ClassifiedClass $class) {
+                return $class->getNumberOfMethods();
+            });
+            $methodsPerClass = round($numberOfMethods / $numberOfClasses, 2);
+
+            $linesOfCode = $classifiedClasses->sum(function (ClassifiedClass $class) {
+                return $class->getLines();
+            });
+
+            $logicalLinesOfCode = $classifiedClasses->sum(function (ClassifiedClass $class) {
+                return $class->getLogicalLinesOfCode();
+            });
+            $logicalLinesOfCodePerMethod = $numberOfMethods === 0 ? 0 : round($logicalLinesOfCode / $numberOfMethods, 2);
+
+
+            $singleComponent = [
+                'name' => $componentName,
+                'number_of_classes' => $numberOfClasses,
+                'number_of_methods' => $numberOfMethods,
+                'methods_per_class' => $methodsPerClass,
+                'loc' => $linesOfCode,
+                'lloc' => $logicalLinesOfCode,
+                'lloc_per_method' => $logicalLinesOfCodePerMethod,
+            ];
+
+            if ($isVerbose === true) {
+
+                $arrayOfClasses = [];
+
+                foreach ($classifiedClasses as $classifiedClass) {
+                    $arrayOfClasses[] = [
+                        'name' => $classifiedClass->reflectionClass->getName(),
+                        'methods' => $classifiedClass->getNumberOfMethods(),
+                        'methods_per_class' => $classifiedClass->getNumberOfMethods(),
+                        'loc' => $classifiedClass->getLines(),
+                        'lloc' => $classifiedClass->getLogicalLinesOfCode(),
+                        'lloc_per_method' => $classifiedClass->getLogicalLinesOfCodePerMethod(),
+                    ];
+                }
+
+                $singleComponent['classes'] = $arrayOfClasses;
+            }
+
+            $jsonStructure['components'][] = $singleComponent;
+        }
+
+        $this->output->text(json_encode($jsonStructure));
     }
+
+    private function groupClassesByComponentName($project): Collection
+    {
+        return $project
+            ->classifiedClasses()
+            ->groupBy(function ($classifiedClass) {
+                return $classifiedClass->classifier->name();
+            })
+            ->sortBy(function ($_, $componentName) {
+                return $componentName;
+            });
+    }
+
 
     /**
      * Render output from given statistics.
      *
+     * @deprecated
      * @param  \Wnx\LaravelStats\Statistics\ProjectStatistics $statistics
      * @return void
      */
