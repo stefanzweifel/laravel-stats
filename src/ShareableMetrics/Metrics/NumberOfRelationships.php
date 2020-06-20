45c1cc99da9539af210606b276b5c67fdaa77123
Stefan Zweifel <hello@stefanzweifel.io>
2020-06-20 15:41:02 +0200

    Add NumberOfRelationships Metric (WIP)
    This Metric only yields result, if the relationship methods have a type hint with a Laravel Relationship.
    
    If no type hint is added, we can't detect if the method returns a relationship or not. (Or at least I need to dig deeper of what's possible with Reflection)

---
+++ src/ShareableMetrics/Metrics/NumberOfRelationships.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Wnx\LaravelStats\ShareableMetrics\Metrics;
+
+use Illuminate\Support\Str;
+use Wnx\LaravelStats\Classifiers\ModelClassifier;
+use Wnx\LaravelStats\Contracts\CollectableMetric;
+use Wnx\LaravelStats\ValueObjects\ClassifiedClass;
+use Wnx\LaravelStats\ValueObjects\Component;
+
+class NumberOfRelationships extends Metric implements CollectableMetric
+{
+    public function type(): string
+    {
+        return 'numeric';
+    }
+
+    public function name(): string
+    {
+        return 'project_number_of_relationships';
+    }
+
+    public function value()
+    {
+        return $this->project
+            ->classifiedClasses()
+            ->filter(function (ClassifiedClass $classifiedClass) {
+                return $classifiedClass->classifier->name() === 'Models';
+            })
+            ->map(function (ClassifiedClass $classifiedClass) {
+                return $classifiedClass->reflectionClass->getMethods();
+            })
+            ->flatten(1)
+            ->filter(function (\ReflectionMethod $method) {
+                return $method->hasReturnType();
+            })
+            ->filter(function (\ReflectionMethod $method) {
+                return Str::of($method->getReturnType()->getName())->startsWith('Illuminate\Database\Eloquent\Relations');
+            })
+            ->count();
+    }
+}
---
+++ tests/ShareableMetrics/Metrics/NumberOfRelationshipsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\ShareableMetrics\Metrics;
+
+use Wnx\LaravelStats\Project;
+use Wnx\LaravelStats\ShareableMetrics\Metrics\NumberOfRelationships;
+use Wnx\LaravelStats\Tests\Stubs\Models\Group;
+use Wnx\LaravelStats\Tests\Stubs\Models\Post;
+use Wnx\LaravelStats\Tests\Stubs\Models\User;
+use Wnx\LaravelStats\Tests\TestCase;
+
+class NumberOfRelationshipsTest extends TestCase
+{
+    /** @test */
+    public function it_returns_0_if_no_relationships_could_be_found_for_a_given_project()
+    {
+        $project = $this->createProjectFromClasses([]);
+
+        $metric = new NumberOfRelationships($project);
+
+        $this->assertEquals(0, $metric->value());
+    }
+
+    /** @test */
+    public function it_returns_correct_number_of_relationships_in_a_project_for_a_given_project()
+    {
+        $project = $this->createProjectFromClasses([
+            User::class,
+            Post::class,
+            Group::class
+        ]);
+
+        $metric = new NumberOfRelationships($project);
+
+        $this->assertEquals(5, $metric->value());
+    }
+
+}
