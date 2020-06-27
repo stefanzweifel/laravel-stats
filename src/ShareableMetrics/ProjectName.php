bcec7668d526234c64a3099e61f94585fc93c3bd
Stefan Zweifel <hello@stefanzweifel.io>
2020-06-27 16:35:14 +0200

    Update how ProjectName is determined

---
+++ src/ShareableMetrics/ProjectName.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace Wnx\LaravelStats\ShareableMetrics;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Symfony\Component\Process\Process;
+
+class ProjectName
+{
+    public const RC_FILE = '.laravelstatsrc';
+
+    public function get(): ?string
+    {
+        if ($this->hasStoredProjectName()) {
+            return File::get($this->pathToRcFile());
+        }
+
+        return null;
+    }
+
+    public function determineProjectNameFromGit(): ?string
+    {
+        $process = Process::fromShellCommandline('/usr/local/bin/git config --get remote.origin.url');
+
+        $process->run();
+
+        if ($process->isSuccessful() === false) {
+            return null;
+        }
+
+        $remoteUrl = parse_url(trim($process->getOutput()));
+
+        return Str::of($remoteUrl['path'])
+            ->replaceFirst('/', '')
+            ->__toString();
+    }
+
+    protected function pathToRcFile(): string
+    {
+        return base_path(self::RC_FILE);
+    }
+
+    public function hasStoredProjectName(): bool
+    {
+        return File::exists($this->pathToRcFile());
+    }
+
+    public function storeNameInRcFile(string $projectName): void
+    {
+        File::put($this->pathToRcFile(), $projectName);
+    }
+}
