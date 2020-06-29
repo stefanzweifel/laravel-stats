04f557ba3ce1f1e67b8ae883b7fe68708beb0825
Stefan Zweifel <hello@stefanzweifel.io>
2020-06-29 21:05:34 +0200

    Add ControllerFormRequestInjection

---
+++ src/ShareableMetrics/Metrics/ControllerFormRequestInjection.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Wnx\LaravelStats\ShareableMetrics\Metrics;
+
+use Illuminate\Foundation\Http\FormRequest;
+use ReflectionMethod;
+use ReflectionParameter;
+use ReflectionProperty;
+use Wnx\LaravelStats\Contracts\CollectableMetric;
+use Wnx\LaravelStats\ReflectionClass;
+use Wnx\LaravelStats\ValueObjects\ClassifiedClass;
+
+class ControllerFormRequestInjection extends Metric implements CollectableMetric
+{
+    public function name(): string
+    {
+        return 'controllers_form_request_injection';
+    }
+
+    public function value()
+    {
+        return $this->project
+            ->classifiedClasses()
+            ->filter(function (ClassifiedClass $classifiedClass) {
+                return $classifiedClass->classifier->name() === 'Controllers';
+            })
+            ->map(function (ClassifiedClass $classifiedClass) {
+
+                $methods = collect($classifiedClass->reflectionClass->getDefinedMethods());
+
+                return $methods->filter(function (ReflectionMethod $method) {
+                        return count($method->getParameters()) > 0;
+                    })
+                    ->filter(function (ReflectionMethod $method) {
+                        return collect($method->getParameters())
+                            ->filter(function (ReflectionParameter $param) {
+                                return $param->hasType();
+                            })
+                            ->filter(function (ReflectionParameter $param) {
+
+                                $reflectionClass = new ReflectionClass($param->getType()->getName());
+
+                                return $reflectionClass->isSubclassOf(FormRequest::class);
+                            });
+                    })
+                    ->count() > 0;
+            })->count() > 0;
+    }
+}
---
+++ tests/ShareableMetrics/Metrics/ControllerFormRequestInjectionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\ShareableMetrics\Metrics;
+
+use Illuminate\Support\Facades\Route;
+use Wnx\LaravelStats\ShareableMetrics\Metrics\ControllerFormRequestInjection;
+use Wnx\LaravelStats\Tests\Stubs\Controllers\ProjectsController;
+use Wnx\LaravelStats\Tests\Stubs\Controllers\UsersController;
+use Wnx\LaravelStats\Tests\TestCase;
+
+class ControllerFormRequestInjectionTest extends TestCase
+{
+    /** @test */
+    public function it_returns_correct_metric_name()
+    {
+        $project = $this->createProjectFromClasses([]);
+
+        $metric = new ControllerFormRequestInjection($project);
+
+        $this->assertEquals('controllers_form_request_injection', $metric->name());
+    }
+
+    /** @test */
+    public function it_returns_true_if_controllers_do_use_form_request_injection()
+    {
+        Route::get('users', [UsersController::class, 'index']);
+
+        $project = $this->createProjectFromClasses([
+            UsersController::class
+        ]);
+
+        $metric = new ControllerFormRequestInjection($project);
+
+        $this->assertTrue($metric->value());
+    }
+
+    /** @test */
+    public function it_returns_false_if_controllers_do_not_use_form_request_injection()
+    {
+        $this->markTestIncomplete("This test shouldn't fail.");
+
+        Route::get('projects', [ProjectsController::class, 'index']);
+
+        $project = $this->createProjectFromClasses([
+            ProjectsController::class
+        ]);
+
+        $metric = new ControllerFormRequestInjection($project);
+
+        $this->assertFalse($metric->value());
+    }
+}
