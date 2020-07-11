2f700362f8746eae954e43ae63aacad065837c91
Stefan Zweifel <hello@stefanzweifel.io>
2020-07-11 11:59:54 +0200

    Add UserObserver Stub

---
+++ tests/Stubs/Observers/UserObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Stubs\Observers;
+
+use Wnx\LaravelStats\Tests\Stubs\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
