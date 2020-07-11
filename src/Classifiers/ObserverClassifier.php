43603327f9c66a76dc1c87b6f6a3ec51be4007d8
Stefan Zweifel <hello@stefanzweifel.io>
2020-07-11 12:00:05 +0200

    Add ObserverClassifier

---
+++ src/Classifiers/ObserverClassifier.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace Wnx\LaravelStats\Classifiers;
+
+use Closure;
+use Illuminate\Support\Str;
+use ReflectionFunction;
+use ReflectionProperty;
+use Wnx\LaravelStats\ReflectionClass;
+use Wnx\LaravelStats\Contracts\Classifier;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class ObserverClassifier implements Classifier
+{
+    public function name(): string
+    {
+        return 'Observers';
+    }
+
+    public function satisfies(ReflectionClass $class): bool
+    {
+        return collect($this->getEvents())
+            ->filter(function ($listeners, $event) {
+                return Str::of($event)->startsWith('eloquent.');
+            })
+            ->map(function ($listeners) {
+                return collect($listeners)->map(function (Closure $closure) {
+                    return $this->getEventListener($closure);
+                })->toArray();
+            })
+            ->collapse()
+            ->filter(function ($eventListenerSignature) use ($class) {
+               return Str::of($eventListenerSignature)->contains($class->getName());
+            })
+            ->count() > 0;
+    }
+
+    protected function getEvents()
+    {
+        $dispatcher = app('events');
+
+        $property = new ReflectionProperty($dispatcher, 'listeners');
+        $property->setAccessible(true);
+
+        return $property->getValue($dispatcher);
+    }
+
+    protected function getEventListener(Closure $closure)
+    {
+        $reflection = new ReflectionFunction($closure);
+
+        return $reflection->getStaticVariables()['listener'];
+    }
+
+    public function countsTowardsApplicationCode(): bool
+    {
+        return true;
+    }
+
+    public function countsTowardsTests(): bool
+    {
+        return false;
+    }
+}
