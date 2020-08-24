3eebc7329300cb8dae028d19498e4fa8f9c2a0e0
Stefan Zweifel <hello@stefanzweifel.io>
2020-08-24 20:34:36 +0200

    Add AllMetricPayloadKeys
    This Class is mostly used for internal development purposes.

---
+++ src/ShareableMetrics/AllMetricPayloadKeys.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Wnx\LaravelStats\ShareableMetrics;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Wnx\LaravelStats\Classifier;
+use Wnx\LaravelStats\Project;
+use Wnx\LaravelStats\ShareableMetrics\CollectMetrics;
+
+class AllMetricPayloadKeys
+{
+    public function get(): array
+    {
+        return $this->getProjectMetricKeys()
+            ->merge($this->getComponentMetricKeys())
+            ->toArray();
+    }
+
+    private function getProjectMetricKeys(): Collection
+    {
+        return collect(CollectMetrics::PROJECT_METRICS)
+            ->map(function ($metricClass) {
+                return new $metricClass(new Project(collect([])));
+            })
+            ->map(function ($metric) {
+                return $metric->name();
+            });
+    }
+
+    private function getComponentMetricKeys(): Collection
+    {
+        $coreClassifierNames = array_map(function ($classifier) {
+            return (new $classifier)->name();
+        }, Classifier::DEFAULT_CLASSIFIER);
+
+        $metrics = [];
+
+        /** @var Component $component */
+        foreach ($coreClassifierNames as $coreClassifier) {
+            $slug = Str::slug(strtolower($coreClassifier), '_');
+
+            $metrics[] = [
+                "{$slug}",
+                "{$slug}_methods",
+                "{$slug}_loc",
+                "{$slug}_lloc",
+                "{$slug}_lloc_per_method",
+            ];
+        }
+
+        return collect($metrics)->flatten();
+    }
+}
