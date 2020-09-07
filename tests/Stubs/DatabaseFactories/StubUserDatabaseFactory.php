e4b48fd80376d17677273144f11ea9a528370fd9
Stefan Zweifel <hello@stefanzweifel.io>
2020-09-07 20:08:08 +0200

    Add DatabaseFactoryClassifier
    Closes #181

---
+++ tests/Stubs/DatabaseFactories/StubUserDatabaseFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wnx\LaravelStats\Tests\Stubs\DatabaseFactories;
+
+use Illuminate\Database\Eloquent\Factory;
+use Wnx\LaravelStats\Tests\Stubs\Models\User;
+
+class StubUserDatabaseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+}
