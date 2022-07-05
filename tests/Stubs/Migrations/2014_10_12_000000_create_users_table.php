034c05e7236018600948b9f2aae8bac0202a2301
Stefan Zweifel <hello@stefanzweifel.io>
2022-07-05 20:47:27 +0200

    Make User Migration anonymous

--- tests/Stubs/Migrations/2014_10_12_000000_create_users_table.php
+++ tests/Stubs/Migrations/2014_10_12_000000_create_users_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -16,9 +16,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -34,4 +35,5 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
-}
+};
+
