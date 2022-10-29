aebc8ef4f2e30f2bdae693b114b2b7dbf72e1425
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2022-10-29 07:44:15 +0200

    Add Support for PHP 8.2 (#210)
    * Update Workflow

--- tests/Classifiers/ObserverClassifierTest.php
+++ tests/Classifiers/ObserverClassifierTest.php
@@ -10,7 +10,6 @@ use Wnx\LaravelStats\ReflectionClass;
 
 class ObserverClassifierTest extends TestCase
 {
-
     /** @test */
     public function it_returns_true_if_given_class_is_a_registered_observer()
     {
